bd633692a7735dff7539774c6f2bdafc744949e9
letruongluat <lluat91@gmail.com>
2024-06-25 15:19:39 +0700

    Add project files.

---
+++ QuanLyQuanCafe/fAccountProfile.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace QuanLyQuanCafe
+{
+    partial class fAccountProfile
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txbUserName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txbDisplayName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnExti = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.txbUserName);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(18, 18);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(591, 68);
+            this.panel2.TabIndex = 1;
+            // 
+            // txbUserName
+            // 
+            this.txbUserName.Location = new System.Drawing.Point(208, 15);
+            this.txbUserName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txbUserName.Name = "txbUserName";
+            this.txbUserName.ReadOnly = true;
+            this.txbUserName.Size = new System.Drawing.Size(376, 26);
+            this.txbUserName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(4, 14);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(193, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tên đăng nhập:";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txbDisplayName);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(18, 95);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(591, 68);
+            this.panel1.TabIndex = 2;
+            // 
+            // txbDisplayName
+            // 
+            this.txbDisplayName.Location = new System.Drawing.Point(208, 15);
+            this.txbDisplayName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txbDisplayName.Name = "txbDisplayName";
+            this.txbDisplayName.Size = new System.Drawing.Size(376, 26);
+            this.txbDisplayName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label2.Location = new System.Drawing.Point(4, 14);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(157, 29);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Tên hiển thị:";
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(370, 403);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(112, 35);
+            this.btnUpdate.TabIndex = 6;
+            this.btnUpdate.Text = "Cập nhật";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnExti
+            // 
+            this.btnExti.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExti.Location = new System.Drawing.Point(492, 403);
+            this.btnExti.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnExti.Name = "btnExti";
+            this.btnExti.Size = new System.Drawing.Size(112, 35);
+            this.btnExti.TabIndex = 7;
+            this.btnExti.Text = "Thoát";
+            this.btnExti.UseVisualStyleBackColor = true;
+            this.btnExti.Click += new System.EventHandler(this.btnExti_Click);
+            // 
+            // fAccountProfile
+            // 
+            this.AcceptButton = this.btnUpdate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExti;
+            this.ClientSize = new System.Drawing.Size(634, 455);
+            this.Controls.Add(this.btnExti);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "fAccountProfile";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Thông tin cá nhân";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox txbUserName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txbDisplayName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnExti;
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# Do_An_CNPM
+--Tên các Thành Viên
+NHÓM 4:    
+CÙ THỊ NGỌC ANH - 25%
+NGUYỄN VĂN ĐẠI - 25%
+LÊ TRƯỜNG LUẬT - 25%
+HOÀNG VIỆT NHẬT - 25%
+
+
+--Cách để chạy Source Code
+B1. Tải Project về máy
+B2. Vào Project và lấy file BackUp_File_QLOrder của DataBase 
+B3. Restore DataBase vào SQL Server
+B4. Sau đó chỉnh lại đường dẫn ở App.Config giống đường dẫn kết nối SQl Server và tên thư mục
+B5. Chạy Source Code
